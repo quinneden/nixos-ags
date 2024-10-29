2daa9d87b32f11e81996d744cfc655e68982a27f
Quinn Edenfield <quinnyxboy@gmail.com>
2024-10-29 03:12:03 -0700

    Modified: flake.nix

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
       inputs.hyprland.follows = "hyprland";
     };
 
-    matugen.url = "github:InioX/matugen"; # ?ref=v2.2.0
+    matugen.url = "github:InioX/matugen";
     ags.url = "github:Aylur/ags";
     astal.url = "github:Aylur/astal";
 
